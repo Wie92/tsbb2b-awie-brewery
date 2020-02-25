ebe12187bd69493063c3ef0a91acdcbfdc61ad2a
awie <awie@debian.debian>
2020-02-25 20:44:44 +0700

    First commit

---
+++ src/main/java/com/awie/brewery/TsbbAwieBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.awie.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsbbAwieBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsbbAwieBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/awie/brewery/TsbbAwieBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awie.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsbbAwieBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
